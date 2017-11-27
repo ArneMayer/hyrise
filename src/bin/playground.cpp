968c81be7ead98bf2d678c968d868a865194c970
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2017-11-27 19:04:33 +0100

    benchmark refactoring

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <cstdlib>
 
 #include "types.hpp"
 #include "operators/get_table.hpp"
@@ -16,6 +18,21 @@
 #include <json.hpp>
 
 using namespace opossum;
+
+void print_table_layout(std::string table_name) {
+  auto table = StorageManager::get().get_table(table_name);
+  std::cout << "table: " << table_name << std::endl;
+  std::cout << "rows: " << table->row_count() << std::endl;
+  std::cout << "chunks: " << table->chunk_count() << std::endl;
+  std::cout << "columns: " << table->column_count() << std::endl;
+  for (auto column_id = ColumnID{0}; column_id < table->column_count(); column_id++) {
+    auto column_name = table->column_name(column_id);
+    auto column_type = table->column_type(column_id);
+    std::cout << "(" << column_type << ") " << column_name << ", ";
+  }
+  std::cout << std::endl;
+  std::cout << "------------------------" << std::endl;
+}
 
 template <typename T>
 void analyze_value_interval(std::string table_name, std::string column_name) {
@@ -51,54 +68,119 @@ void print_chunk_sizes(std::shared_ptr<const Table> table) {
   }
 }
 
-void tpcc_load_or_generate(int warehouse_size, int chunk_size, std::string table_name) {
-  auto sstream = std::stringstream();
-  sstream << table_name << warehouse_size << "_" << chunk_size;
-  auto file_name = sstream.str();
-  bool file_exists = std::ifstream(file_name).good();
+/**
+* Checks whether the table exist in the storage manager or on disk
+*/
+bool load_table(std::string table_name) {
+  if (StorageManager::get().has_table(table_name)) {
+    return true;
+  }
+  bool file_exists = std::ifstream(table_name).good();
   if (file_exists) {
-    std::cout << " > Loading from disk" << std::endl;
-    auto import = std::make_shared<ImportBinary>(file_name, table_name);
+    std::cout << " > Loading table " << table_name << " from disk" << std::endl;
+    auto import = std::make_shared<ImportBinary>(table_name, table_name);
     import->execute();
+    return true;
   } else {
-    std::cout << " > Generating from scratch" << std::endl;
-    auto table = tpcc::TpccTableGenerator::generate_tpcc_table(table_name, chunk_size, warehouse_size);
+    return false;
+  }
+}
+
+void save_table(std::shared_ptr<const Table> table, std::string file_name) {
+  std::cout << " > Saving table " << file_name << " to disk" << std::endl;
+  auto table_wrapper = std::make_shared<TableWrapper>(table);
+  table_wrapper->execute();
+  auto export_operator = std::make_shared<ExportBinary>(table_wrapper, file_name);
+  export_operator->execute();
+}
+
+std::string tpcc_load_or_generate(int warehouse_size, int chunk_size, std::string tpcc_table_name) {
+  auto table_name = tpcc_table_name + "_" + std::to_string(warehouse_size) + "_" + std::to_string(chunk_size);
+  auto loaded = load_table(table_name);
+  if (!loaded) {
+    std::cout << " > Generating table " << table_name << std::endl;
+    auto table = tpcc::TpccTableGenerator::generate_tpcc_table(tpcc_table_name, chunk_size, warehouse_size);
     StorageManager::get().add_table(table_name, table);
-    auto table_wrapper = std::make_shared<TableWrapper>(table);
-    table_wrapper->execute();
-    auto export_operator = std::make_shared<ExportBinary>(table_wrapper, file_name);
-    export_operator->execute();
+    save_table(table, table_name);
   }
+
+  return table_name;
 }
 
-void generate_data(int warehouse_size, int chunk_size, std::string benchmark_table_name) {
-  std::cout << "TPCC" << std::endl;
-  std::cout << " > Generating tables" << std::endl;
-  tpcc_load_or_generate(warehouse_size, chunk_size, benchmark_table_name);
+int random_int(int min, int max, int except) {
+  DebugAssert(min <= max, "min value must be <= max value");
+  DebugAssert(!(min == max && max == except), "value range empty");
 
-  // Print table information
-  auto table_names = StorageManager::get().table_names();
-  for (auto table_name : table_names) {
-    auto table = StorageManager::get().get_table(table_name);
-    //print_chunk_sizes(table);
-    std::cout << "table: " << table_name << std::endl;
-    std::cout << "rows: " << table->row_count() << std::endl;
-    std::cout << "chunks: " << table->chunk_count() << std::endl;
-    std::cout << "columns: " << table->column_count() << std::endl;
-    for (auto column_id = ColumnID{0}; column_id < table->column_count(); column_id++) {
-      auto column_name = table->column_name(column_id);
-      auto column_type = table->column_type(column_id);
-      std::cout << "(" << column_type << ") " << column_name << ", ";
+  int value;
+  do {
+    value = std::rand() % (max - min) + min;
+  } while (value == except);
+
+  return value;
+}
+
+std::vector<AllTypeVariant> generate_row(int scan_column, int scan_column_value, int column_count) {
+  auto row = std::vector<AllTypeVariant>(column_count);
+  for (int i = 0; i < column_count; i++) {
+    row[i] = 0;
+  }
+  row[scan_column] = scan_column_value;
+
+  return row;
+}
+
+std::string best_case_load_or_generate(int row_count, int chunk_size) {
+  auto table_name = "best_case_" + std::to_string(row_count) + "_" + std::to_string(chunk_size);
+  auto loaded = load_table(table_name);
+  if (loaded) {
+    return table_name;
+  }
+
+  std::cout << " > Generating table " << table_name << std::endl;
+
+  // Requirements:
+  // - 10 columns minimum,
+  // - Every chunk is compressed
+
+  // Best case: Dictionary can prune nothing, filter can prune everything
+  // -> Scan value in between min/max
+  // -> No chunk contains the actual scan value
+  const auto column_count = 10;
+  const auto scan_column = 0;
+  const auto scan_value = 3000;
+  const auto min_value = 0;
+  const auto max_value = 200000;
+
+  // Generate table header
+  auto table = std::make_shared<Table>(chunk_size);
+  for (int i = 0; i < column_count; i++) {
+    table->add_column_definition("column" + i, "int", false);
+  }
+
+  // Generate table data
+  for (int row_number = 0; row_number < row_count; row_number++) {
+    if (row_number % chunk_size == 0) {
+      table->append(generate_row(scan_column, min_value, column_count));
+    } else if (row_number % chunk_size == 1) {
+      table->append(generate_row(scan_column, max_value, column_count));
+    } else {
+      table->append(generate_row(scan_column, random_int(min_value, max_value, scan_value), column_count));
     }
-    std::cout << std::endl;
-    std::cout << "------------------------" << std::endl;
-
   }
-  std::cout << " > Done" << std::endl;
+
+  StorageManager::get().add_table(table_name, table);
+  save_table(table, table_name);
+
+  return table_name;
 }
 
-std::shared_ptr<AbstractOperator> generate_benchmark(std::string table_name, std::string column_name,
-                                                     uint8_t quotient_size, uint8_t remainder_size) {
+void worst_case_load_or_generate(int rows, int chunk_size) {
+}
+
+std::shared_ptr<AbstractOperator> generate_benchmark(std::string tpcc_table_name, std::string column_name,
+                                                     uint8_t quotient_size, uint8_t remainder_size, int warehouse_size,
+                                                     int chunk_size) {
+  auto table_name = tpcc_load_or_generate(warehouse_size, chunk_size, tpcc_table_name);
   auto table = StorageManager::get().get_table(table_name);
   auto column_id = table->column_id_by_name(column_name);
   if (remainder_size > 0 && quotient_size > 0) {
@@ -131,8 +213,8 @@ void serialize_results(nlohmann::json results) {
 }
 
 int main() {
-  auto table_name = "ORDER";
-  auto column_name = "NO_O_ID";
+  auto tpcc_table_name = std::string("ORDER");
+  auto column_name = std::string("NO_O_ID");
   auto warehouse_size = 100;
   auto chunk_size = 100000;
   auto quotient_size = 14;
@@ -141,24 +223,24 @@ int main() {
   //auto quotient_sizes = {0, 10, 16};
   nlohmann::json results;
   results["results"] = nlohmann::json::array();
-  results["table_name"] = table_name;
+  results["table_name"] = tpcc_table_name;
   results["column_name"] = column_name;
   results["warehouse_size"] = warehouse_size;
   results["chunk_size"] = chunk_size;
 
   std::cout << "------------------------" << std::endl;
   std::cout << "Benchmark configuration: " << std::endl;
-  std::cout << "table_name:     " << table_name << std::endl;
+  std::cout << "table_name:     " << tpcc_table_name << std::endl;
   std::cout << "column_name:    " << column_name << std::endl;
   std::cout << "warehouse_size: " << warehouse_size << std::endl;
   std::cout << "chunk_size:     " << chunk_size << std::endl;
   std::cout << "quotient_size:  " << quotient_size << std::endl;
   std::cout << "------------------------" << std::endl;
 
-  generate_data(warehouse_size, chunk_size, table_name);
-  analyze_value_interval<int>(table_name, column_name);
+  // analyze_value_interval<int>(table_name, column_name);
   for (auto remainder_size : remainder_sizes) {
-    auto benchmark = generate_benchmark(table_name, column_name, quotient_size, remainder_size);
+    auto benchmark = generate_benchmark(tpcc_table_name, column_name, quotient_size, remainder_size,
+                                        warehouse_size, chunk_size);
     auto start = std::chrono::steady_clock::now();
     benchmark->execute();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start);
@@ -173,8 +255,8 @@ int main() {
               << std::endl;
   }
 
-  //std::cout << results << std::endl;
   serialize_results(results);
+  StorageManager::get().reset();
 
   return 0;
 }
