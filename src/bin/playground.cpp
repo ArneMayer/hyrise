c0e2b302ccbf60a6cdce726819b5c81d74d209bd
Arne Mayer <arne.mayer@student.hpi.de>
2017-12-28 20:45:08 +0200

    typo

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -337,7 +337,7 @@ void benchmark_series() {
   auto chunk_sizes = {1'000'000};
   auto pruning_rate = 0.5;
   auto selectivity = 1.0 / 3000.0;
-  auto scan_types = {std::string("int"),std::string("string")};
+  auto scan_types = {std::string("int"), std::string("string")};
 
   std::cout << "------------------------" << std::endl;
   std::cout << "Benchmark configuration: " << std::endl;
@@ -360,7 +360,7 @@ void benchmark_series() {
   results_table->add_column("run_time", DataType::Int, false);
 
   // analyze_value_interval<int>(table_name, column_name);
-  for (auto scan_type : scan_types) {}
+  for (auto scan_type : scan_types) {
     for (auto row_count : row_counts) {
       for (auto chunk_size : chunk_sizes) {
         auto dictionary = false;
