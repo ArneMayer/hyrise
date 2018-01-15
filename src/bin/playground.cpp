821a7bc99886b9e860166e9b05393e8fdfcf5d20
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2018-01-16 00:04:34 +0100

    change variance accordingly

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -697,7 +697,7 @@ void cardinality_misestimation_series(std::string distribution_type, int distinc
 
   std::vector<uint> distribution;
   if (distribution_type == "normal") {
-    double variance = 500.0;
+    double variance = double variance = distinct_values / 6.0;
     distribution = generate_normal_distribution(value_count, distinct_values, variance);
   } else if (distribution_type == "uniform") {
     distribution = generate_uniform_distribution(value_count, distinct_values);
@@ -780,7 +780,7 @@ void filter_cardinality_estimation_series(std::string distribution_type, int dis
 
   std::vector<uint> distribution;
   if (distribution_type == "normal") {
-    double variance = 1000.0;
+    double variance = distinct_values / 6.0;
     distribution = generate_normal_distribution(value_count, distinct_values, variance);
   } else if (distribution_type == "uniform") {
     distribution = generate_uniform_distribution(value_count, distinct_values);
