6c9eed432d65538b7c9522803c8403fba8889be1
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2018-01-30 00:56:34 +0100

    filters

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -38,11 +38,13 @@ int main() {
   //tpcc_benchmark_series();
   //jcch_benchmark_series();
   auto table_name = jcch_load_or_generate("LINEITEM", 6000000, 100000, false);
+  table = StorageManager::get().get_table(table_name);
+  create_quotient_filters(table, table->column_id_by_name("L_SHIPDATE"), 17, 4);
   auto prunable_actual = analyze_skippable_chunks_actual(table_name, "L_SHIPDATE", std::string("1992-02-24"));
   auto prunable_filter = analyze_skippable_chunks_filter(table_name, "L_SHIPDATE", std::string("1992-02-24"));
   std::cout << "prunable actual: " << prunable_actual << std::endl;
   std::cout << "prunable filter: " << prunable_filter << std::endl;
-  std::cout << "total chunks: " << StorageManager::get().get_table(table_name)->chunk_count() << std::endl;
+  std::cout << "total chunks: " << table->chunk_count() << std::endl;
   //acdoca_benchmark_series();
   //dict_vs_filter_series_cached();
   //dict_vs_filter_series_uncached();
