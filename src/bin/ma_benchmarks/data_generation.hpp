6bdc854655d4899b7fe68b6767beab66075e6fa7
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2018-02-18 03:29:21 +0100

    forgot

--- src/bin/ma_benchmarks/data_generation.hpp
+++ src/bin/ma_benchmarks/data_generation.hpp
@@ -364,7 +364,7 @@ std::string acdoca_load_or_generate(std::string column_name, int row_count, int 
   auto column_type = complete_table->column_type(column_id);
   table->add_column_definition(column_name, column_type, false);
   auto actual_row_count = complete_table->row_count();
-  for (int row_number = 0; row_number < actual_row_count; row_number++) {
+  for (uint64_t row_number = 0; row_number < actual_row_count; row_number++) {
     AllTypeVariant value;
     if (column_type == DataType::Int) {
       value = complete_table->get_value<int>(column_id, row_number);
