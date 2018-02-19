1453a83393d7979b24511029a4a5484f748874e5
Arne Mayer <arne.mayer@student.hpi.uni-potsdam.de>
2018-02-19 02:10:47 +0100

    acdoca data fix

--- src/bin/ma_benchmarks/data_generation.hpp
+++ src/bin/ma_benchmarks/data_generation.hpp
@@ -362,7 +362,7 @@ std::string acdoca_load_or_generate(std::string column_name, int row_count, int 
   auto table = std::make_shared<Table>(chunk_size);
   auto column_id = complete_table->column_id_by_name(column_name);
   auto column_type = complete_table->column_type(column_id);
-  table->add_column_definition(column_name, column_type, false);
+  table->add_column(column_name, column_type, false);
   auto actual_row_count = complete_table->row_count();
   std::cout << "Complete table row count: " << actual_row_count << std::endl;
   for (uint64_t row_number = 0; row_number < actual_row_count; row_number++) {
