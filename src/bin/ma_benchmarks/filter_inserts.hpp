a43de64697ef366c7ae97830022a16b8e647d6b3
Arne Mayer <arne.mayer@student.hpi.de>
2018-04-26 00:06:14 +0200

    fix

--- src/bin/ma_benchmarks/filter_inserts.hpp
+++ src/bin/ma_benchmarks/filter_inserts.hpp
@@ -5,8 +5,9 @@
 
 double inserts_per_second_string() {
   const int n = 100'000;
+  const int filter_size = n * 100;
   std::string values[n];
-  uint8_t quotient_size = static_cast<uint8_t>(std::ceil(std::log2(n)));
+  uint8_t quotient_size = static_cast<uint8_t>(std::ceil(std::log2(filter_size)));
   uint8_t remainder_size = 8;
   for (int i = 0; i < n; i++) {
     values[i] = random_string(16);
@@ -15,20 +16,21 @@ double inserts_per_second_string() {
 
   clear_cache();
   auto start = std::chrono::steady_clock::now();
-  for (int i = 0; i < n; i++) {
-    filter.insert(values[i]);
+  for (int i = 0; i < filter_size; i++) {
+    filter.insert(values[i % n]);
   }
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()-start);
-  auto inserts_per_second = n / static_cast<double>(duration.count()) * 1'000'000;
+  auto inserts_per_second = filter_size / static_cast<double>(duration.count()) * 1'000'000;
 
-  std::cout << inserts_per_second << " string inserts per second";
+  std::cout << inserts_per_second << " string inserts per second" << std::endl;
   return inserts_per_second;
 }
 
 double inserts_per_second_int() {
   const int n = 100'000;
+  const int filter_size = n * 100;
   int values[n];
-  uint8_t quotient_size = static_cast<uint8_t>(std::ceil(std::log2(n)));
+  uint8_t quotient_size = static_cast<uint8_t>(std::ceil(std::log2(filter_size)));
   uint8_t remainder_size = 8;
   for (int i = 0; i < n; i++) {
     values[i] = random_int(0, 1'000'000);
@@ -37,12 +39,12 @@ double inserts_per_second_int() {
 
   clear_cache();
   auto start = std::chrono::steady_clock::now();
-  for (int i = 0; i < n; i++) {
-    filter.insert(values[i]);
+  for (int i = 0; i < filter_size; i++) {
+    filter.insert(values[i % n]);
   }
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()-start);
-  auto inserts_per_second = n / static_cast<double>(duration.count()) * 1'000'000;
+  auto inserts_per_second = filter_size / static_cast<double>(duration.count()) * 1'000'000;
 
-  std::cout << inserts_per_second << " int inserts per second";
+  std::cout << inserts_per_second << " int inserts per second" << std::endl;
   return inserts_per_second;
 }
